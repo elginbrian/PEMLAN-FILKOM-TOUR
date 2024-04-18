af1fb40f9580cf0d57f70c831e648f521b599c74
elgin <elginbrian49@gmail.com>
2024-04-18 21:41:55 +0700

    adding RentalController

--- controller/RentalController.java
+++ controller/RentalController.java
@@ -1,5 +1,72 @@
 package controller;
 
+import model.CarModel;
+import model.EmployeeModel;
+
 public class RentalController {
-    
+    private CarModel[] carList;
+    private EmployeeModel[] employeeList;
+    // private CustomerModel[] customerList;
+
+    public RentalController(){}
+
+    public void createCar(CarModel newCar){
+
+    }
+
+    public void readCar(String numPlate){
+
+    }
+
+    public void updateCar(
+        String numPlate,
+        String carBrand,
+        String carColor, 
+        int year,
+        Double tankCapacity
+    ){
+
+    }
+
+    public void deleteCar(String numPlate){
+
+    }
+
+    public void createCustomer(){
+
+    }
+
+    public void readCustomer(String customerId){
+
+    }
+
+    public void updateCustomer(
+        String name,
+        String phoneNum,
+        String address,
+        String gender
+    ){
+
+    }
+
+    public void deleteCustomer(){
+
+    }
+
+    public void rentCar(
+        String customerId,
+        String numPlate,
+        int day
+    ){
+
+    }
+
+    public void returnCar(
+        String customerId,
+        String numPlate,
+        Double rateCar,
+        Double rateEmployee
+    ){
+
+    }
 }
