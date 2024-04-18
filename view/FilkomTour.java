12e4559d3e16bd6161ef1f3e35dd846981f6a352
Jundi Muhammad Fauzan <127640125+Foazan@users.noreply.github.com>
2024-04-18 20:41:49 +0700

    menambahkan p aorhudofliafo

--- view/FilkomTour.java
+++ view/FilkomTour.java
@@ -4,5 +4,6 @@ public class FilkomTour{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello world 2");
+        System.out.println("p");
     }
 }
