b167eac6df64f9e65c387a7b56e573cbc417c91c
elgin <elginbrian49@gmail.com>
2024-04-18 19:22:20 +0700

    hello world 2

--- view/FilkomTour.java
+++ view/FilkomTour.java
@@ -1,9 +1,8 @@
-package view;
-
 import controller.RentalController;
 
 public class FilkomTour{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello world 2");
     }
 }
