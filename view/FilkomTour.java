3a9a26afdcbb030b2546adceade62985a85bc05c
hillan tazidi <160846687+hillan17@users.noreply.github.com>
2024-04-18 19:17:26 +0700

    nambah hello world

--- view/FilkomTour.java
+++ view/FilkomTour.java
@@ -1,6 +1,9 @@
+package view;
+
 import controller.RentalController;
+
 public class FilkomTour{
     public static void main(String[] args) {
-        
+        System.out.println("Hello World!");
     }
 }
